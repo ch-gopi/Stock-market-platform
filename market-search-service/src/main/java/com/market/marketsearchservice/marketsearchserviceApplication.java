f9395732174d6e6eb858faca572cb75d5d5f60b0
gopi78938 <Chowdam.Gopi@apexon.com>
2025-12-15 11:14:05 +0530

    updated redis configuration for classes

--- market-search-service/src/main/java/com/market/marketsearchservice/marketsearchserviceApplication.java
+++ market-search-service/src/main/java/com/market/marketsearchservice/marketsearchserviceApplication.java
@@ -2,6 +2,7 @@ package com.market.marketsearchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableRetry
 @EnableScheduling
 @EnableFeignClients
+@EnableCaching
 @SpringBootApplication
 public class marketsearchserviceApplication {
     public static void main(String[] args) {
